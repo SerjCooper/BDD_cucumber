8f2d65d0e273ea11769ec5bd09d13295d6de4641
Bondarev Serj <b-sergi@mail.ru>
2020-08-23 19:25:22 +0500

    integrated allure report

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,7 +1,9 @@
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
+@Test
 @CucumberOptions(
         plugin = {"html:target/cucumber/html", "json:target/cucumber/json/cucumber.json", "pretty"},
         features = {"src/test/java/features"},
